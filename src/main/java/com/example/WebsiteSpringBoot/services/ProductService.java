4ccfa4f874e7c2784e2703755ae0c7027fcacbe4
PochkaVirta <cyberdog771@gmail.com>
2025-05-21 18:55:22 +0300

    repair project and migrated to Java 21

--- src/main/java/com/example/WebsiteSpringBoot/services/ProductService.java
+++ src/main/java/com/example/WebsiteSpringBoot/services/ProductService.java
@@ -6,32 +6,30 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ProductService {
-//    private final ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
     public List<Product> listProducts(String title) {
-//        if (title != null) productRepository.findByTitle(title);
-//        return productRepository.findAll();
-        return null;
+        if (title != null) {
+            return productRepository.findByTitle(title);
+        }
+        return productRepository.findAll();
     }
 
     public void saveProduct(Product product) {
-//        log.info("Saving new {}", product);
-//        productRepository.save(product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-//        productRepository.deleteById(id);
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-//        return productRepository.findById(id).orElse(null);
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
