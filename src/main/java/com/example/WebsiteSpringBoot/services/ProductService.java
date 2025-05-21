fa8a387e73484b251ed52178d9c14afecf7f1b13
PochkaVirta <cyberdog771@gmail.com>
2025-05-21 17:28:45 +0300

    added mySql

--- src/main/java/com/example/WebsiteSpringBoot/services/ProductService.java
+++ src/main/java/com/example/WebsiteSpringBoot/services/ProductService.java
@@ -1,38 +1,37 @@
 package com.example.WebsiteSpringBoot.services;
 
 import com.example.WebsiteSpringBoot.models.Product;
+import com.example.WebsiteSpringBoot.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> products = new ArrayList<Product>();
-    private long ID = 0;
+//    private final ProductRepository productRepository;
 
-    {
-        products.add(new Product(++ID, "PlayStation 5", "Simple description", 67000, "Krasnoyarsk", "tomas"));
-        products.add(new Product(++ID, "Iphone 8", "Simple description", 24000, "Moscow", "artmcoder"));
-    }
-
-    public List<Product> listProducts() {
-        return products;
+    public List<Product> listProducts(String title) {
+//        if (title != null) productRepository.findByTitle(title);
+//        return productRepository.findAll();
+        return null;
     }
 
     public void saveProduct(Product product) {
-        product.setId(++ID);
-        products.add(product);
+//        log.info("Saving new {}", product);
+//        productRepository.save(product);
     }
 
     public void deleteProduct(Long id) {
-        products.removeIf(product -> product.getId().equals(id));
+//        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : products) {
-            if (product.getId().equals(id)) return product;
-        }
+//        return productRepository.findById(id).orElse(null);
         return null;
     }
 }
